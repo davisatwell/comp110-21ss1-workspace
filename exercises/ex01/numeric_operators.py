63a97c22a7b5214aff1c4825dd1fb5dae8edda84
Your Name <davisatw@email.unc.edu>
2021-05-20 00:37:43 -0400

    Tweaked EX01

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,13 +1,13 @@
-"""Demonstrating **, /, //, and % """
+"""Demonstrating **, /, //, and %."""
 
 __author__: str = "730384155"
 
 left: str = input("Left-hand side:")
 right: str = input("Right-hand side:")
-sol1 = int(left)**int(right)
-sol2 = int(left)/int(right)
-sol3 = int(left)//int(right)
-sol4 = int(left)%int(right)
+sol1 = int(left) ** int(right)
+sol2 = int(left) / int(right)
+sol3 = int(left) // int(right)
+sol4 = int(left) % int(right)
 print(str(left), "**", str(right), "is", str(sol1))
 print(str(left), "/", str(right), "is", str(sol2))
 print(str(left), "//", str(right), "is", str(sol3))
