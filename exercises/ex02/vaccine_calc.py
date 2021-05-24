815ef91329ff38557319b23cacf6cb399f38855a
Davis Atwell <davisatw@email.unc.edu>
2021-05-24 14:38:06 -0400

    pushed cahnges

--- exercises/ex02/vaccine_calc.py
+++ exercises/ex02/vaccine_calc.py
@@ -16,32 +16,28 @@ from datetime import datetime
 # Official Documentation: https://docs.python.org/3/library/datetime.html#datetime.timedelta
 from datetime import timedelta
 
-#Global Vars needed for calculator
+# Global Vars needed for calculator
 population_input: str = input("Population: ")
 population: int = int(population_input)
 # Note: 2 doses will cover one person
 doses_administered_input: str = input("Doses administered: ")
 doses_administered: int = int(doses_administered_input)
-
 doses_per_day_input: str = input("Doses per day: ")
 doses_per_day: int = int(doses_per_day_input)
-
 target_percent_vaccinated_input: str = input("Target percent vaccinated: ")
 target_percent_vaccinated: int = int(target_percent_vaccinated_input)
-
 today: datetime = datetime.today()
 one_day: timedelta = timedelta(1)
-#Start of program
-dec_target: float = target_percent_vaccinated/100
-target_doses: float = population*dec_target
-target_doses: float = target_doses*2
-target_doses: float = target_doses - doses_administered
-
-days_to_goal: int = round(target_doses/doses_per_day)
-
+# Start of program
+dec_target: float = target_percent_vaccinated / 100
+target_doses: float = population * dec_target
+target_doses1: float = target_doses * 2
+target_doses2: float = target_doses1 - doses_administered
+days_to_goal: int = round(target_doses2 / doses_per_day)
 days_to_goal1: timedelta = timedelta(days_to_goal)
-
 day_goal_is_reached: datetime = today + days_to_goal1
-date_: datetime = day_goal_is_reached.strftime("%B %d, %Y")
-
-print("We will reach",str(target_percent_vaccinated)+"%","vaccination in",str(days_to_goal),"days, which falls on", str(date_))
+date_: str = day_goal_is_reached.strftime("%B %d, %Y")
+prcnt: str = str(target_percent_vaccinated)
+days: str = str(days_to_goal)
+date: str = str(date_)
+print("We will reach", prcnt + "%", "vaccination in", days, "days, which falls on", date)
