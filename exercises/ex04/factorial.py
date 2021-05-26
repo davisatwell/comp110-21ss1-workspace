696bc3f816809163768a474a835e1483292f0a01
Davis Atwell <davisatw@email.unc.edu>
2021-05-26 15:12:02 -0400

    Ex04 Part One done

--- exercises/ex04/factorial.py
+++ exercises/ex04/factorial.py
@@ -2,4 +2,13 @@
 
 __author__ = "730384155"
 
+user_input: int = int(input("Pick a number to factorial: "))
 
+i: int = 1
+factorial: int = 1
+
+while i <= user_input:
+    factorial *= i
+    i += 1    
+else:
+    print("Factorial is:", (str(factorial)))
