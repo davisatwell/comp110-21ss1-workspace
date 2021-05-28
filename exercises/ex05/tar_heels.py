cab329dae5291266a668c75fa76e7caaad8c0f33
Davis Atwell <davisatw@email.unc.edu>
2021-05-27 20:23:02 -0400

    update ex05

--- exercises/ex05/tar_heels.py
+++ exercises/ex05/tar_heels.py
@@ -7,24 +7,25 @@ from random import choice
 i: int = 0
 j: int = 1
 
-
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
     print(tar_heels(choice))
 
-def tar_heels(Ramsees: int)-> str:
-        s: str = ""
-        if Ramsees % 2 == 0:
-            if Ramsees % 7 == 0:
-                s += "TAR HEELS"
-            else:
-                s += "TAR"
-        elif Ramsees % 7 == 0:
-            s += "HEELS"
+
+def tar_heels(Ramsees: int) -> str:
+    """UNC"""
+    s: str = ""
+    if Ramsees % 2 == 0:
+        if Ramsees % 7 == 0:
+            s += "TAR HEELS"
         else:
-            s += "CAROLINA"
-        return s
+            s += "TAR"
+    elif Ramsees % 7 == 0:
+        s += "HEELS"
+    else:
+        s += "CAROLINA"
+    return s
 
 
 if __name__ == "__main__":
