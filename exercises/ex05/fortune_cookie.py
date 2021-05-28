4d3aac30c4c58a09ef7130cd48e8937df400a799
Davis Atwell <davisatw@email.unc.edu>
2021-05-27 20:12:38 -0400

    ex05 done [updated]

--- exercises/ex05/fortune_cookie.py
+++ exercises/ex05/fortune_cookie.py
@@ -8,13 +8,14 @@ __author__ = "730384155"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    print(fortune)
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
 
 i: int = 0
 j: int = 1
-num: int = randint(0,2)
+
 def fortune_cookie()-> str: 
+    num: int = randint(0,2)
     s: str = ""
     if num == 0:
         s += "You will find the love of your life soon"
@@ -23,7 +24,7 @@ def fortune_cookie()-> str:
     else:
         s += "You will be given UNC basketball tickets next season!"
     return s
-fortune = fortune_cookie()
+
 
 # Python Idiom for "starting" the program when run as a module.
 # The special dunder variable __name__ will be "__main__" when run as module. 
