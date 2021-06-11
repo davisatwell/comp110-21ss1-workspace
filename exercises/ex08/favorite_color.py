6d0a402c43be3907d11c23cf52fce6e25e70802a
Davis Atwell <davisatw@email.unc.edu>
2021-06-10 22:40:07 -0400

    ex08 done v1.0

--- exercises/ex08/favorite_color.py
+++ exercises/ex08/favorite_color.py
@@ -11,20 +11,22 @@ def main() -> None:
 color_count: dict[str, int] = {"davis": "red", "leo": "red", "rhett": "blue"}
 frequency_table: dict[str, int] = {}
 
+
 def favorite_color(names_and_colors: dict[str, str]) -> str:
     """returns color that appears most in names_and_colors."""
     s: str = ""
+    i: int = 0
     for key in names_and_colors:
         color = names_and_colors[key]
         if color in frequency_table:
             frequency_table[color] += 1
         else:
             frequency_table[color] = 1
-    print(frequency_table)
-    # for color in frequency_table:
-    #     if frequency_table[key] > values:
-    #         values = frequency_table[key]
-    #s += str(values)
+    # print(frequency_table)
+    for key in frequency_table:
+        if frequency_table[key] > i:
+            i = frequency_table[key]
+            s = key
     return s
 
 
